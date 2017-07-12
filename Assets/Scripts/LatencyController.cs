cca065e1a6eeae9ef67998ebbe180ed47d4f1871
decapent <lavalleep@gmail.com>
2017-07-12 19:51:57 -0400

    Drum integration hype Latency and FPS in game not in casque !

---
+++ Assets/Scripts/LatencyController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LatencyController : MonoBehaviour
+{
+
+    public Text fps;
+    public Text latency;
+
+    float deltaTime = 0.0f;
+
+    OVRDisplay.LatencyData latencyData;
+
+    void Start()
+    {
+        latencyData = OVRManager.display.latency;
+    }
+
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+
+        latency.text = string.Format("Latency : {0:0.00} ms", latencyData.render);            
+
+        fps.text = string.Format("FPS : {0}", deltaTime);
+    }
+}
