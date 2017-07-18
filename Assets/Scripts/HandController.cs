2405c9bf71867854421a1d4bd1ce8b3bac4d8dff
Patrick Lavallée <patrick.lavallee@gsoft.com>
2017-07-17 22:39:58 -0400

    exercice 3 oculus touch plumbing

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(OVRInput.Get(OVRInput.Button.PrimaryHandTrigger))
+        {
+            this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }	
+        else
+        {
+            this.gameObject.GetComponent<Renderer>().material.color = Color.gray;
+        }
+	}
+}
