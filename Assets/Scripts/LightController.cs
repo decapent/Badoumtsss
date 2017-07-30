9a9e9fc4aff4951e8ba04cc31b57775ce19f8469
alloLaPlanete <simonolivierharel@gmail.com>
2017-07-30 13:02:52 -0400

    Nouvel environnement.

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Light light;
+    // Use this for initialization
+    void Start () {
+        light = this.GetComponent<Light>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Flashlight"))
+        {
+            
+            light.gameObject.SetActive(false);
+
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/SwithLightController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwithLightController : MonoBehaviour {
+
+    public Light light;
+    bool lightIsOpen = true;
+
+    // Use this for initialization
+    void Start() {
+        this.light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Flashlight"))
+        {
+            if (lightIsOpen)
+                SwitchLight(false);
+            else
+                SwitchLight(true);
+        }
+
+    }
+
+    void SwitchLight(bool isActive)
+    {
+        light.enabled=isActive;
+        lightIsOpen = isActive;
+    }
+}
